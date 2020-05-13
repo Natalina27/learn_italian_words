3c2a4bf3b7176cf36046a7e3de84c878c1e2f4fc
Natalya Myunster <natalyamyunster@yahoo.com>
2020-05-13 15:53:07 +0200

    Homework 2

--- src/components/HeaderBlock/HeaderBlock.js
+++ src/components/HeaderBlock/HeaderBlock.js
@@ -8,7 +8,7 @@ const HeaderBlock = () => {
            <div className={styles.wrap}>
                <h1 className={styles.header}>Learn English words online</h1>
                <ReactLogoSvg />
-               <p className={styles.descr}> Use fill out cards and replenish your vocabulary</p>
+               <p className={styles.decr}> Use fill out cards and replenish your vocabulary</p>
            </div>
        </div>
     );
