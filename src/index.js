35e9bb92be97fd9ac2cfb196f0ee2cef5441d955
Natalya Myunster <natalyamyunster@yahoo.com>
2020-05-13 11:50:50 +0200

    JSX syntax

--- src/index.js
+++ src/index.js
@@ -2,21 +2,37 @@ import React from "react";
 import ReactDom from 'react-dom';
 
 const AppList = () => {
+    const items = ['Item1', 'Item2','Item3', 'Item4'];
+    const title = <h1>My list</h1>;
+
+    const isAuth = false;
+
     return (
         <ul>
-            <li>My first list</li>
-            <li>My second list</li>
+            { isAuth ? title : null }
+            { items.map(item => <li>{item}</li>) }
         </ul>
     )
 };
 
 const AppHeader = () => {
-    return <h1>Hello World!!!</h1>
+    return <h1 className='header'>Hello World!!!</h1>
 };
+
+const AppInput = () => {
+    const placeholder = 'Type text...';
+    return (
+        <label htmlFor='search'>
+            <input placeholder={placeholder}/>
+        </label>
+    )
+};
+
 const App = () => {
     return(
         <>
             <AppHeader />
+            <AppInput/>
             <AppList />
             <AppHeader />
             <AppList />
