febcdac66463c8941c8f223a0c2f51989a2deeab
Natalya Myunster <natalyamyunster@yahoo.com>
2020-05-18 02:46:00 +0200

    Updates

---
+++ precache-manifest.ccdbe0d0db64cbc4834ca2068ff0d274.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d963591cdc7f3bf484b843ef43818c",
+    "url": "/learn_italian_words/index.html"
+  },
+  {
+    "revision": "b9045a81407521906065",
+    "url": "/learn_italian_words/static/css/main.4ba2e200.chunk.css"
+  },
+  {
+    "revision": "74b5baf8aa494365d231",
+    "url": "/learn_italian_words/static/js/2.eaea840a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learn_italian_words/static/js/2.eaea840a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9045a81407521906065",
+    "url": "/learn_italian_words/static/js/main.fe2a58e5.chunk.js"
+  },
+  {
+    "revision": "40bbcfa8cdb95e946b53",
+    "url": "/learn_italian_words/static/js/runtime-main.ef394e53.js"
+  },
+  {
+    "revision": "858cfb16cd8d67ad70818d1cd4499df8",
+    "url": "/learn_italian_words/static/media/florence.858cfb16.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/learn_italian_words/precache-manifest.e530563262ae37574a1b64389a42f376.js"
+  "/learn_italian_words/precache-manifest.ccdbe0d0db64cbc4834ca2068ff0d274.js"
 );
 
 self.addEventListener('message', (event) => {
