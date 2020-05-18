6a4757fa0e9db84dd12478313f3ab69c4b3a49a5
Natalya Myunster <natalyamyunster@yahoo.com>
2020-05-18 02:39:20 +0200

    Updates

---
+++ precache-manifest.e530563262ae37574a1b64389a42f376.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f2a428dd8e7ddffff36c22d0965e0a",
+    "url": "/learn_italian_words/index.html"
+  },
+  {
+    "revision": "8467030c1240bd9b7b86",
+    "url": "/learn_italian_words/static/js/2.512200f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learn_italian_words/static/js/2.512200f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "360b088d7b45b7abb264",
+    "url": "/learn_italian_words/static/js/main.428de1c9.chunk.js"
+  },
+  {
+    "revision": "40bbcfa8cdb95e946b53",
+    "url": "/learn_italian_words/static/js/runtime-main.ef394e53.js"
+  }
+]);
